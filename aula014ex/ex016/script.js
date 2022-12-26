5154475968aa0b2fcce9e138b4df655c21836b79
Rafael cruz <110062923+DevRafaelCruz@users.noreply.github.com>
2022-12-25 21:49:26 -0300

    Mais um exercício finalizado

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -1,1 +1,12 @@
- 
+function contar(){
+    var inicio = Number(document.getElementById('idinicio').value)
+    var fim = Number(document.getElementById('idfim').value)
+    var passo = Number(document.getElementById('idpasso').value)
+    var res = document.getElementById('res')
+    var restxt = document.getElementById('restxt')
+    restxt.innerHTML = ('Contando...')
+
+    for(inicio;inicio<=fim;inicio+=passo){
+        res.innerHTML+=(inicio + ',') 
+    }
+} 
