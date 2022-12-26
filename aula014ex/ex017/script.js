005dbe56072f6e802d7ee547e930eed9a2d2e54c
Rafael cruz <110062923+DevRafaelCruz@users.noreply.github.com>
2022-12-25 22:56:06 -0300

    tabuada quase pronta

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -1,1 +1,10 @@
- 
+function gerarTabuada(){
+    var num = Number(document.getElementById('inum').value)
+    var res = document.getElementById('res')
+
+    for(var i=0;i<=10;i++){
+        var n = num*i
+        res.innerHTML+=(`${num} x ${i} = ${n}`)
+        res.innerHTML+=('<br>')
+    }
+}
