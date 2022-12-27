b4b58318a04ead388bd8391e900d48f8645acbff
Rafael cruz <110062923+DevRafaelCruz@users.noreply.github.com>
2022-12-27 19:27:40 -0300

    Curso finalizado!

--- aula016ex/modelo/script.js
+++ aula016ex/modelo/script.js
@@ -1,21 +1,62 @@
-function adicionar(){
-    var sel = document.getElementById('idsel')
-    var num = Number(document.getElementById('idnum').value)
-    var nums = []
-    var inp = document.getElementById('idnum')
-
-    if(num<= 0 || num > 100){
-        alert('Número inválido! O valor inserido deve estar entre 1 e 100')
+let num = document.getElementById('idnum')
+let sel = document.getElementById('flista')
+var res = document.getElementById('res')
+let nums = []
+ 
+function isnum(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
     }else{
-        nums.push(num)
-        var item = document.createElement('option')
-        item.text = `Valor ${num} adicionado`
-        sel.appendChild(item)
-        inp.innerHTML = ''
+        return false
     }
 }
 
-function finalizar(){
-    
+function inlista(n,lis){
+    if(lis.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
 }
 
+function adicionar(){
+    if(isnum(num.value) && !inlista(num.value, nums)){
+        nums.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        sel.appendChild(item)
+        res.innerHTML=''
+    }else{
+        alert('Valor inválido ou já encrontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if(nums.length==0){
+        alert('Adicione calores antes de finalizar!')
+    }else{
+        let tot = nums.length
+        let maior = nums[0]
+        let menor = nums[0]
+        let soma = 0
+        for(pos in nums){
+            soma+=nums[pos]
+            if(nums[pos]>maior){
+                maior = nums[pos]
+            }
+            if(nums[pos]<menor){
+                menor = nums[pos]
+            }
+        }
+        media=soma/tot    
+
+        res.innerHTML=''
+        res.innerHTML+=`<p>Ao todo, temos ${tot} números cadastrados</p>`
+        res.innerHTML+=`<p>O maior valor inserido foi ${maior}.</p>`
+        res.innerHTML+=`<p>O maior valor inserido foi ${menor}.</p>`
+        res.innerHTML+=`<p>A soma dos valores é ${soma}.</p>`
+        res.innerHTML+=`<p>A media dos valores é ${media}.</p>`
+    }
+}
